44441b821045f16062a3de712f09ea18b9069506
Saroj Senadheera <ooxygen89@gmail.com>
2019-02-20 23:57:30 +0530

    Created basic login function

--- app/src/main/java/com/example/whatsupcolombo/LoginActivity.java
+++ app/src/main/java/com/example/whatsupcolombo/LoginActivity.java
@@ -1,12 +1,21 @@
 package com.example.whatsupcolombo;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,6 +23,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText Password;
     private Button Login;
     private TextView SignUp;
+    private FirebaseAuth firebaseAuth; //import auth libs of firebase
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,15 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         setUpUI();
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        //check if a user is alredy loggedin or not
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            //destroys the activity
+            finish();
+            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+        }
         SignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,10 +64,23 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void validate(String userNmae, String userPassword) {
-        if ((userNmae.equals("Saroj") && (userPassword.equals("12345")))) {
-            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-            startActivity(intent);
-
-        }
+        //create message until the verification is done
+        progressDialog.setMessage("please wait");
+        //show message
+        progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(Username.toString(), Password.toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    //remove message as teh verification is done
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+                }else{
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this,"Login Failed",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
