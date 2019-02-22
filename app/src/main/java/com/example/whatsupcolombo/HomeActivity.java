5d3707f3a23b9697e59cf3b9ea994044f0481c3f
Saroj Senadheera <ooxygen89@gmail.com>
2019-02-22 14:48:17 +0530

    Created Navigation

--- app/src/main/java/com/example/whatsupcolombo/HomeActivity.java
+++ app/src/main/java/com/example/whatsupcolombo/HomeActivity.java
@@ -1,13 +1,93 @@
 package com.example.whatsupcolombo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        //link to xml
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.ui_container) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_profile) {
+            //displays the particular fragment
+            getSupportFragmentManager().beginTransaction().replace(R.id.ui_base, new Profile_UI()).commit();
+        } else if (id == R.id.nav_suggest) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.ui_base, new Suggestion_UI()).commit();
+        } else if (id == R.id.nav_event) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.ui_base, new Event_UI()).commit();
+        } else if (id == R.id.nav_restaurent) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.ui_base, new Restaurent_UI()).commit();
+        } else if (id == R.id.nav_logout) {
+            Toast.makeText(HomeActivity.this, "Looged out", Toast.LENGTH_SHORT).show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
